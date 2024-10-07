cfbbd490bdf3600018505a8eaa44a23168deb5d5
Adrian Emmott <adrian.emmott@gmail.com>
2024-10-07 03:15:20 +0100

    Added identity

--- Ratbags.Gateway/Program.cs
+++ Ratbags.Gateway/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ratbags.Shared.DTOs.Events.AppSettingsBase;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var environment = builder.Environment;
 
 // secrets
 if (builder.Environment.IsDevelopment())
@@ -10,6 +14,11 @@ if (builder.Environment.IsDevelopment())
     builder.Configuration.AddUserSecrets<Program>();
 }
 
+builder.Services.Configure<AppSettingsBase>(builder.Configuration);
+// hmm
+//builder.Configuration
+//    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)  // Base settings
+//    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);  // Environment-specific
 
 builder.Services.Configure<AppSettingsBase>(builder.Configuration);
 var appSettings = builder.Configuration.Get<AppSettingsBase>() ?? throw new Exception("Appsettings missing");
@@ -42,25 +51,46 @@ builder.Services.AddCors(options =>
             .AllowCredentials());
 });
 
+// config ocelot for local / docker
+//builder.Configuration.AddJsonFile("ocelot.json");
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"ocelot.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+    
+
 // add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// config ocelot
-builder.Configuration.AddJsonFile("ocelot.json");
-
 // setup identity with ocelot (this proj)
 builder.Services.AddAuthentication()
     .AddJwtBearer("IdentityApiKey", options =>
     {
-        options.Authority = "https://localhost:5000"; // Identity Server or Auth endpoint
-        options.Audience = "api";
+        options.Authority = "https://localhost:7158"; // account api
+        options.Audience = "builder.Configuration[\"JWT:Audience\"]"; 
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
     });
 
 builder.Services.AddOcelot(builder.Configuration);
-
 var app = builder.Build();
+
+var ocelotConfig = builder.Configuration.GetSection("Routes").GetChildren();
+foreach (var route in ocelotConfig)
+{
+    app.Logger.LogInformation($"Downstream Host: {route["DownstreamHostAndPorts:0:Host"]}");
+    app.Logger.LogInformation($"Downstream Port: {route["DownstreamHostAndPorts:0:Port"]}");
+}
 
 // cors
 app.UseCors("AllowSpecificOriginAngular");
@@ -77,14 +107,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
-// "/" Hello World!
-app.MapGet("/", async context =>
-{
-    await context.Response.WriteAsync("Hello World!");
-});
 
 // controllers
 app.MapControllers();
